6dfbaa2830161c39bb1eee887ef0a3eb9c084e59
Eduardo Almada <edu.almada26@gmail.com>
2022-11-14 17:20:16 -0300

    feat: consuming pokemon api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,70 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import * as S from './styles'
+import { useNavigation } from '@react-navigation/native'
+import { propStack } from '../../routes/Models'
+import api from '../../services/api'
+import { Text } from 'react-native'
+
+type PokemonType = {
+  type: string
+}
+
+type Pokemon = {
+  name: string,
+  url: string,
+  id: string,
+  types: PokemonType[]
+}
+
+type Request = {
+  id: number,
+  types: PokemonType[]
+}
 
 const Home = () => {
+  const navigation = useNavigation<propStack>()
+
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    async function getAllPokemons() {
+      const response = await api.get('/pokemon')
+      const { results } = response.data;
+
+      const payloadPokemons = await Promise.all(
+        results.map(async (pokemon: Pokemon) => {
+          const { id, types } = await getMoreInfo(pokemon.url)
+
+          return {
+            name: pokemon.name,
+            id,
+            types,
+          }
+
+        })
+
+      )
+      setPokemons(payloadPokemons)
+
+    }
+
+    getAllPokemons()
+  }, [])
+
+  async function getMoreInfo(url: string): Promise<Request> {
+    const response = await api(url)
+    const { id, types } = response.data;
+
+    return { id, types }
+  }
+
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+
+    <S.Container>
+      {pokemons.map(item =><Text>{item.name}</Text>)}
+    </S.Container>
   )
+
 }
 
 export default Home
