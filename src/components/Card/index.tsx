9ffac1e554670f8aafb7626a8c1ddcc22b2623cf
Eduardo Almada <edu.almada26@gmail.com>
2022-11-23 14:59:05 -0300

    feat:Create home header

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { TouchableOpacityProps } from 'react-native';
 import dotsImage from '../../assets/img/dots.png'
 import * as S from './styles'
-import Pokeball from  '../../assets/img/pokeballCard.png'
+import Pokeball from '../../assets/img/pokeballCard.png'
+import FadeAnimation from '../FadeAnimation';
 export type PokemonType = {
   type: {
     name: string;
@@ -29,22 +30,25 @@ const Card = ({ data, ...rest }: Props) => {
         <S.PokemonName>{data.name}</S.PokemonName>
         <S.ImageDots source={dotsImage} />
         <S.PokemonContentType>
-          {data.types.map(pokemonType => 
-          <S.PokemonType type={pokemonType.type.name}>
-            <S.PokemonTypeText key={pokemonType.type.name}>
-              {pokemonType.type.name}
-            </S.PokemonTypeText>
-          </S.PokemonType>
+          {data.types.map(pokemonType =>
+            <S.PokemonType type={pokemonType.type.name}>
+              <S.PokemonTypeText key={pokemonType.type.name}>
+                {pokemonType.type.name}
+              </S.PokemonTypeText>
+            </S.PokemonType>
           )}
 
         </S.PokemonContentType>
       </S.LeftSide>
       <S.RightSide>
-        <S.PokeballDetail source={Pokeball}/>
-        <S.PokemonImage source={{
-          uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
-        }}/>
+        <FadeAnimation>
+          <S.PokeballDetail source={Pokeball} />
+          <S.PokemonImage source={{
+            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+          }} />
+        </FadeAnimation>
       </S.RightSide>
+
     </S.PokemonCard>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,20 +3,23 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Welcome from '../pages/Welcome'
 import Home from '../pages/Home'
 import { propsNavigationStack } from './Models'
+import Information from '../pages/Information'
 
 
 const Stack = createStackNavigator<propsNavigationStack>()
 
 const Routes = () => {
     return (
-        <Stack.Navigator initialRouteName='Welcome'>
+        <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown:false}}>
             <Stack.Screen
                 name='Welcome'
-                component={Welcome}
-                options={{ headerShown: false }} />
+                component={Welcome}/>
             <Stack.Screen
                 name='Home'
                 component={Home}/>
+            <Stack.Screen
+                name='Information'
+                component={Information}/>
         </Stack.Navigator>
     )
 }
