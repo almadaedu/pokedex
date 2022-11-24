3c09c9a03923893dd496d32a749b90d0724c01ef
Eduardo Almada <edu.almada26@gmail.com>
2022-11-24 00:34:40 -0300

    feat: Show pokemon Id in respective card

--- src/pages/Information/index.tsx
+++ src/pages/Information/index.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {useRoute, useNavigation} from '@react-navigation/native'
+
+type RouteParams = {
+  pokemonId: number
+}
 
 const Information = () => {
+  const route = useRoute();
+  const {pokemonId} = route.params as RouteParams;
+  
   return (
     <View>
-      <Text>styles</Text>
+      <Text style={{margin: 100}}>{pokemonId}</Text>
     </View>
   )
 }
